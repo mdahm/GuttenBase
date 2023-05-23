15044701e7ed1a8b721757ebf6adf15c4a195a51
Markus Dahm <markus.dahm@akquinet.de>
2023-05-23 16:41:20 +0200

    Regard precision only if not defined in meta data

--- src/main/java/de/akquinet/jbosscc/guttenbase/mapping/DefaultColumnTypeMapper.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/mapping/DefaultColumnTypeMapper.java
@@ -17,8 +17,9 @@ import static de.akquinet.jbosscc.guttenbase.connector.DatabaseType.*;
  * &copy; 2012-2034 akquinet tech@spree
  * </p>
  */
+@SuppressWarnings("ALL")
 public class DefaultColumnTypeMapper implements ColumnTypeMapper {
-  private final Map<DatabaseType, Map<DatabaseType, Map<String, String>>> _mappings = new HashMap<>();
+  private final Map<DatabaseType, Map<DatabaseType, Map<String, ColumnDefinition>>> _mappings = new HashMap<>();
 
   public DefaultColumnTypeMapper() {
     createPostgresToMysqlMapping();
@@ -48,15 +49,15 @@ public class DefaultColumnTypeMapper implements ColumnTypeMapper {
   public String mapColumnType(final ColumnMetaData columnMetaData,
                               final DatabaseType sourceDatabaseType, final DatabaseType targetDatabaseType) {
     final String columnType = columnMetaData.getColumnTypeName().toUpperCase();
-    final String targetColumnType = getMapping(sourceDatabaseType, targetDatabaseType, columnType);
+    final ColumnDefinition columnDefinition = getMapping(sourceDatabaseType, targetDatabaseType, columnType);
     final String notNull = columnMetaData.isNullable() ? "" : " NOT NULL";
 
-    if (targetColumnType != null) {
-      final String precision = createPrecisionClause(columnMetaData);
+    if (columnDefinition != null) {
+      final String precision = createPrecisionClause(columnMetaData, columnDefinition.getPrecision());
 
-      return targetColumnType + precision + notNull;
+      return columnDefinition.getType() + precision + notNull;
     } else {
-      return getColumnType(columnMetaData) + notNull;
+      return getColumnType(columnMetaData, "") + notNull;
     }
   }
 
@@ -65,14 +66,14 @@ public class DefaultColumnTypeMapper implements ColumnTypeMapper {
    *
    * @return target database type including precision
    */
-  protected String getColumnType(final ColumnMetaData columnMetaData) {
-    final String precision = createPrecisionClause(columnMetaData);
+  protected String getColumnType(final ColumnMetaData columnMetaData, final String optionalPrecision) {
+    final String precision = createPrecisionClause(columnMetaData, optionalPrecision);
     final String defaultColumnType = columnMetaData.getColumnTypeName();
 
     return defaultColumnType + precision;
   }
 
-  protected String createPrecisionClause(final ColumnMetaData columnMetaData) {
+  protected String createPrecisionClause(final ColumnMetaData columnMetaData, final String optionalPrecision) {
     switch (columnMetaData.getColumnType()) {
       case Types.CHAR:
       case Types.VARCHAR:
@@ -85,15 +86,15 @@ public class DefaultColumnTypeMapper implements ColumnTypeMapper {
           return precision > 0 ? "(" + precision + ")" : "";
         }
       default:
-        return "";
+        return optionalPrecision;
     }
   }
 
-  public String getMapping(final DatabaseType sourceDB, final DatabaseType targetDB, final String columnType) {
-    final Map<DatabaseType, Map<String, String>> databaseMatrix = _mappings.get(sourceDB);
+  public ColumnDefinition getMapping(final DatabaseType sourceDB, final DatabaseType targetDB, final String columnType) {
+    final Map<DatabaseType, Map<String, ColumnDefinition>> databaseMatrix = _mappings.get(sourceDB);
 
     if (databaseMatrix != null) {
-      final Map<String, String> databaseMapping = databaseMatrix.get(targetDB);
+      final Map<String, ColumnDefinition> databaseMapping = databaseMatrix.get(targetDB);
 
       if (databaseMapping != null) {
         return databaseMapping.get(columnType);
@@ -104,22 +105,26 @@ public class DefaultColumnTypeMapper implements ColumnTypeMapper {
   }
 
   public final DefaultColumnTypeMapper addMapping(final DatabaseType sourceDB, final DatabaseType targetDB, final String sourceTypeName, String targetTypeName) {
-    addMappingInternal(sourceDB, targetDB, sourceTypeName, targetTypeName);
+    return addMapping(sourceDB, targetDB, sourceTypeName, targetTypeName, "");
+  }
+
+  public final DefaultColumnTypeMapper addMapping(final DatabaseType sourceDB, final DatabaseType targetDB, final String sourceTypeName, String targetTypeName, String precision) {
+    addMappingInternal(sourceDB, targetDB, sourceTypeName, targetTypeName, precision);
 
     if (sourceDB == MYSQL) {
-      addMappingInternal(MARIADB, targetDB, sourceTypeName, targetTypeName);
+      addMappingInternal(MARIADB, targetDB, sourceTypeName, targetTypeName, precision);
     } else if (targetDB == MYSQL) {
-      addMappingInternal(sourceDB, MARIADB, sourceTypeName, targetTypeName);
+      addMappingInternal(sourceDB, MARIADB, sourceTypeName, targetTypeName, precision);
     }
 
     return this;
   }
 
-  private void addMappingInternal(DatabaseType sourceDB, DatabaseType targetDB, String sourceTypeName, String targetTypeName) {
-    final Map<DatabaseType, Map<String, String>> databaseMatrix = _mappings.computeIfAbsent(sourceDB, k -> new HashMap<>());
-    final Map<String, String> mapping = databaseMatrix.computeIfAbsent(targetDB, k -> new HashMap<>());
+  private void addMappingInternal(DatabaseType sourceDB, DatabaseType targetDB, String sourceTypeName, String targetTypeName, String precision) {
+    final Map<DatabaseType, Map<String, ColumnDefinition>> databaseMatrix = _mappings.computeIfAbsent(sourceDB, k -> new HashMap<>());
+    final Map<String, ColumnDefinition> mapping = databaseMatrix.computeIfAbsent(targetDB, k -> new HashMap<>());
 
-    mapping.put(sourceTypeName, targetTypeName);
+    mapping.put(sourceTypeName, new ColumnDefinition(targetTypeName, precision));
   }
 
   private void createPostgresToMysqlMapping() {
@@ -132,13 +137,13 @@ public class DefaultColumnTypeMapper implements ColumnTypeMapper {
     addMapping(POSTGRESQL, MYSQL, "CIDR", "VARCHAR(43)");
     addMapping(POSTGRESQL, MYSQL, "CIRCLE", "POLYGON");
     addMapping(POSTGRESQL, MYSQL, "DOUBLE PRECISION", "DOUBLE");
-    addMapping(POSTGRESQL, MYSQL, "INET", "VARCHAR(43)");
+    addMapping(POSTGRESQL, MYSQL, "INET", "VARCHAR", "(43)");
     addMapping(POSTGRESQL, MYSQL, "INTERVAL", "TIME");
     addMapping(POSTGRESQL, MYSQL, "JSON", "LONGTEXT");
     addMapping(POSTGRESQL, MYSQL, "LINE", "LINESTRING");
     addMapping(POSTGRESQL, MYSQL, "LSEG", "LINESTRING");
-    addMapping(POSTGRESQL, MYSQL, "MACADDR", "VARCHAR(17)");
-    addMapping(POSTGRESQL, MYSQL, "MONEY", "DECIMAL(19,2)");
+    addMapping(POSTGRESQL, MYSQL, "MACADDR", "VARCHAR", "(17)");
+    addMapping(POSTGRESQL, MYSQL, "MONEY", "DECIMAL", "(19,2)");
     addMapping(POSTGRESQL, MYSQL, "NATIONAL CHARACTER VARYING", "VARCHAR");
     addMapping(POSTGRESQL, MYSQL, "NATIONAL CHARACTER", "CHAR");
     addMapping(POSTGRESQL, MYSQL, "NUMERIC", "DECIMAL");
@@ -151,7 +156,7 @@ public class DefaultColumnTypeMapper implements ColumnTypeMapper {
     addMapping(POSTGRESQL, MYSQL, "TSQUERY", "LONGTEXT");
     addMapping(POSTGRESQL, MYSQL, "TSVECTOR", "LONGTEXT");
     addMapping(POSTGRESQL, MYSQL, "TXID_SNAPSHOT", "VARCHAR");
-    addMapping(POSTGRESQL, MYSQL, "UUID", "VARCHAR(36)");
+    addMapping(POSTGRESQL, MYSQL, "UUID", "VARCHAR", "(36)");
     addMapping(POSTGRESQL, MYSQL, "XML", "LONGTEXT");
     addMapping(POSTGRESQL, MYSQL, "OID", "BLOB");
   }
@@ -163,57 +168,45 @@ public class DefaultColumnTypeMapper implements ColumnTypeMapper {
   }
 
   private void createPostgresToOracleMapping() {
-    addMapping(POSTGRESQL, ORACLE, "BPCHAR(1)", "CHAR(1)");
+    addMapping(POSTGRESQL, ORACLE, "BPCHAR", "CHAR", "(1)");
     addMapping(POSTGRESQL, ORACLE, "BPCHAR", "CHAR");
-    addMapping(POSTGRESQL, ORACLE, "INT8", "NUMBER(3)");
-    addMapping(POSTGRESQL, ORACLE, "INT4", "NUMBER(5)");
-    addMapping(POSTGRESQL, ORACLE, "INT16", "NUMBER(10)");
-    addMapping(POSTGRESQL, ORACLE, "BIGSERIAL", "NUMBER(19)");
+    addMapping(POSTGRESQL, ORACLE, "INT8", "NUMBER", "(3)");
+    addMapping(POSTGRESQL, ORACLE, "INT4", "NUMBER", "(5)");
+    addMapping(POSTGRESQL, ORACLE, "INT16", "NUMBER", "(10)");
+    addMapping(POSTGRESQL, ORACLE, "BIGSERIAL", "NUMBER", "(19)");
     addMapping(POSTGRESQL, ORACLE, "OID", "BLOB");
   }
 
   private void createH2ToDerbyMapping() {
-
     addMapping(DatabaseType.H2DB, DatabaseType.DERBY, "LONGTEXT", "CLOB");
     addMapping(DatabaseType.H2DB, DatabaseType.DERBY, "LONGBLOB", "BLOB");
   }
 
   private void createDerbyToH2Mapping() {
-
     addMapping(DatabaseType.DERBY, DatabaseType.H2DB, "LONGTEXT", "CLOB");
     addMapping(DatabaseType.DERBY, DatabaseType.H2DB, "LONGBLOB", "BLOB");
   }
 
   private void createMysqltoDB2Mapping() {
-    //TODO - ergänzen
-    https:
-//www.ibm.com/developerworks/data/library/techarticle/dm-0606khatri/
-
-    addMapping(MYSQL, DatabaseType.DB2, "LONGTEXT", "VARCHAR(4000)"); //CHAR(254)
+    addMapping(MYSQL, DatabaseType.DB2, "LONGTEXT", "VARCHAR", "(4000)"); //CHAR(254)
     addMapping(MYSQL, DatabaseType.DB2, "LONGBLOB", "BLOB"); //CLOB (2G)
-    addMapping(MYSQL, DatabaseType.DB2, "DECIMAL", "DECIMAL(16)"); //CLOB (2G)
+    addMapping(MYSQL, DatabaseType.DB2, "DECIMAL", "DECIMAL", "(16)"); //CLOB (2G)
   }
 
   private void createPostgrestoDB2Mapping() {
 
-    //TODO - ergänzen
-    https:
-//www.ibm.com/developerworks/data/library/techarticle/dm-0606khatri/
-
-    addMapping(POSTGRESQL, DatabaseType.DB2, "TEXT", "VARCHAR(4000)"); //CHAR(254)
+    addMapping(POSTGRESQL, DatabaseType.DB2, "TEXT", "VARCHAR", "(4000)"); //CHAR(254)
     addMapping(POSTGRESQL, DatabaseType.DB2, "BYTEA", "BLOB"); //CLOB (2G) LONGBLOB
-    addMapping(POSTGRESQL, DatabaseType.DB2, "NUMERIC", "DECIMAL(16)");
-    addMapping(POSTGRESQL, DatabaseType.DB2, "INT(2)", "DECIMAL(16)");
-    addMapping(POSTGRESQL, DatabaseType.DB2, "INT(4)", "DECIMAL(16)");
+    addMapping(POSTGRESQL, DatabaseType.DB2, "NUMERIC", "DECIMAL", "(16)");
+    addMapping(POSTGRESQL, DatabaseType.DB2, "INT(2)", "DECIMAL", "(16)");
+    addMapping(POSTGRESQL, DatabaseType.DB2, "INT(4)", "DECIMAL", "(16)");
 
   }
 
   private void createDB2ToMysqlMapping() {
-
-    //TODO - ergänzen
-    addMapping(DatabaseType.DB2, MYSQL, "CHAR", "VARCHAR(4000)"); //CHAR(254)
-    addMapping(DatabaseType.DB2, MYSQL, "CLOB", "LONGBLOB"); //CLOB (2G)
-    addMapping(DatabaseType.DB2, MYSQL, "INTEGER", "INT(11)");
+    addMapping(DatabaseType.DB2, MYSQL, "CHAR", "CHAR");
+    addMapping(DatabaseType.DB2, MYSQL, "CLOB", "LONGBLOB");
+    addMapping(DatabaseType.DB2, MYSQL, "INTEGER", "INT", "(11)");
 
 
   }
@@ -228,7 +221,7 @@ public class DefaultColumnTypeMapper implements ColumnTypeMapper {
 
   private void createMysqlToPostresMapping() {
     addMapping(MYSQL, POSTGRESQL, "BIGINT AUTO_INCREMENT", "BIGSERIAL");
-    addMapping(MYSQL, POSTGRESQL, "BIGINT UNSIGNED", "NUMERIC(20)");
+    addMapping(MYSQL, POSTGRESQL, "BIGINT UNSIGNED", "NUMERIC", "(20)");
     addMapping(MYSQL, POSTGRESQL, "INTEGER UNSIGNED", "BIGINT");
     addMapping(MYSQL, POSTGRESQL, "INT UNSIGNED", "BIGINT");
     addMapping(MYSQL, POSTGRESQL, "MEDIUMINT UNSIGNED", "INTEGER");
@@ -255,28 +248,28 @@ public class DefaultColumnTypeMapper implements ColumnTypeMapper {
 
   private void createMysqlToOracle() {
     addMapping(MYSQL, ORACLE, "BIT", "RAW");
-    addMapping(MYSQL, ORACLE, "BIGINT", "NUMBER(19, 0)");
+    addMapping(MYSQL, ORACLE, "BIGINT", "NUMBER", "(19, 0)");
     addMapping(MYSQL, ORACLE, "DATETIME", "DATE");
-    addMapping(MYSQL, ORACLE, "DECIMAL", "FLOAT (24)");
-    addMapping(MYSQL, ORACLE, "DOUBLE", "FLOAT (24)");
-    addMapping(MYSQL, ORACLE, "DOUBLE PRECISION", "FLOAT (24)");
+    addMapping(MYSQL, ORACLE, "DECIMAL", "FLOAT", "(24)");
+    addMapping(MYSQL, ORACLE, "DOUBLE", "FLOAT", "(24)");
+    addMapping(MYSQL, ORACLE, "DOUBLE PRECISION", "FLOAT", "(24)");
     addMapping(MYSQL, ORACLE, "ENUM", "VARCHAR2");
-    addMapping(MYSQL, ORACLE, "INT", "NUMBER(10, 0)");
-    addMapping(MYSQL, ORACLE, "INTEGER", "NUMBER(10, 0)");
+    addMapping(MYSQL, ORACLE, "INT", "NUMBER", "(10, 0)");
+    addMapping(MYSQL, ORACLE, "INTEGER", "NUMBER", "(10, 0)");
     addMapping(MYSQL, ORACLE, "LONGBLOB", "BLOB");
     addMapping(MYSQL, ORACLE, "LONGTEXT", "CLOB");
     addMapping(MYSQL, ORACLE, "MEDIUMBLOB", "BLOB");
-    addMapping(MYSQL, ORACLE, "MEDIUMINT", "NUMBER(7, 0)");
+    addMapping(MYSQL, ORACLE, "MEDIUMINT", "NUMBER", "(7, 0)");
     addMapping(MYSQL, ORACLE, "MEDIUMTEXT", "CLOB");
     addMapping(MYSQL, ORACLE, "NUMERIC", "NUMBER");
-    addMapping(MYSQL, ORACLE, "REAL", "FLOAT (24)");
+    addMapping(MYSQL, ORACLE, "REAL", "FLOAT", "(24)");
     addMapping(MYSQL, ORACLE, "SET", "VARCHAR2");
-    addMapping(MYSQL, ORACLE, "SMALLINT", "NUMBER(5, 0)");
+    addMapping(MYSQL, ORACLE, "SMALLINT", "NUMBER", "(5, 0)");
     addMapping(MYSQL, ORACLE, "TEXT", "VARCHAR2");
     addMapping(MYSQL, ORACLE, "TIME", "DATE");
     addMapping(MYSQL, ORACLE, "TIMESTAMP", "DATE");
     addMapping(MYSQL, ORACLE, "TINYBLOB", "RAW");
-    addMapping(MYSQL, ORACLE, "TINYINT", "NUMBER(3, 0)");
+    addMapping(MYSQL, ORACLE, "TINYINT", "NUMBER", "(3, 0)");
     addMapping(MYSQL, ORACLE, "TINYTEXT", "VARCHAR2");
     addMapping(MYSQL, ORACLE, "YEAR", "NUMBER");
     addMapping(MYSQL, ORACLE, "VARBINARY", "BYTEA");
@@ -298,51 +291,69 @@ public class DefaultColumnTypeMapper implements ColumnTypeMapper {
   }
 
   private void createMssqlToOracle() {
-    addMapping(MSSQL, ORACLE, "BIGINT", "NUMBER(19)");
+    addMapping(MSSQL, ORACLE, "BIGINT", "NUMBER", "(19)");
     addMapping(MSSQL, ORACLE, "BINARY", "RAW");
-    addMapping(MSSQL, ORACLE, "BIT", "NUMBER(3)");
+    addMapping(MSSQL, ORACLE, "BIT", "NUMBER", "(1)");
     addMapping(MSSQL, ORACLE, "DATETIME", "DATE");
-    addMapping(MSSQL, ORACLE, "DECIMAL", "NUMBER(10)");
-    addMapping(MSSQL, ORACLE, "FLOAT", "FLOAT(49)");
+    addMapping(MSSQL, ORACLE, "DECIMAL", "NUMBER", "(10)");
+    addMapping(MSSQL, ORACLE, "FLOAT", "FLOAT", "(49)");
     addMapping(MSSQL, ORACLE, "IMAGE", "LONG RAW");
-    addMapping(MSSQL, ORACLE, "INTEGER", "NUMBER(10");
-    addMapping(MSSQL, ORACLE, "MONEY", "NUMBER(19,4)");
+    addMapping(MSSQL, ORACLE, "INTEGER", "NUMBER", "(10");
+    addMapping(MSSQL, ORACLE, "MONEY", "NUMBER", "(19,4)");
     addMapping(MSSQL, ORACLE, "NTEXT", "LONG");
     addMapping(MSSQL, ORACLE, "NVARCHAR", "NCHAR");
-    addMapping(MSSQL, ORACLE, "NUMERIC", "NUMBER(10)");
-    addMapping(MSSQL, ORACLE, "REAL", "FLOAT(23)");
+    addMapping(MSSQL, ORACLE, "NUMERIC", "NUMBER", "(10)");
+    addMapping(MSSQL, ORACLE, "REAL", "FLOAT", "(23)");
     addMapping(MSSQL, ORACLE, "SMALL DATETIME", "DATE");
-    addMapping(MSSQL, ORACLE, "SMALL MONEY", "NUMBER(10,4)");
-    addMapping(MSSQL, ORACLE, "SMALLINT", "NUMBER(5)");
+    addMapping(MSSQL, ORACLE, "SMALL MONEY", "NUMBER", "(10,4)");
+    addMapping(MSSQL, ORACLE, "SMALLINT", "NUMBER", "(5)");
     addMapping(MSSQL, ORACLE, "TEXT", "LONG");
     addMapping(MSSQL, ORACLE, "TIMESTAMP", "RAW");
-    addMapping(MSSQL, ORACLE, "TINYINT", "NUMBER(3)");
-    addMapping(MSSQL, ORACLE, "UNIQUEIDENTIFIER", "CHAR(36)");
+    addMapping(MSSQL, ORACLE, "TINYINT", "NUMBER", "(3)");
+    addMapping(MSSQL, ORACLE, "UNIQUEIDENTIFIER", "CHAR", "(36)");
     addMapping(MSSQL, ORACLE, "VARBINARY", "RAW");
     addMapping(MSSQL, ORACLE, "VARCHAR", "VARCHAR2");
   }
 
   private void createMysqlToMssqlMapping() {
-    addMapping(MYSQL, MSSQL, "LONGTEXT", "NVARCHAR(4000)");
+    addMapping(MYSQL, MSSQL, "LONGTEXT", "NVARCHAR", "(4000)");
     addMapping(MYSQL, MSSQL, "LONGBLOB", "VARBINARY");
-    addMapping(MYSQL, MSSQL, "VARCHAR", "NVARCHAR(4000)");
-    addMapping(MYSQL, MSSQL, "DECIMAL", "DECIMAL(38)");
+    addMapping(MYSQL, MSSQL, "VARCHAR", "NVARCHAR", "(4000)");
+    addMapping(MYSQL, MSSQL, "DECIMAL", "DECIMAL", "(38)");
   }
 
   private void createPostgresToMssqlMapping() {
-    addMapping(POSTGRESQL, MSSQL, "TEXT", "NVARCHAR(4000)");
+    addMapping(POSTGRESQL, MSSQL, "TEXT", "NVARCHAR", "(4000)");
     addMapping(POSTGRESQL, MSSQL, "BYTEA", "BINARY");
     addMapping(POSTGRESQL, MSSQL, "INT4", "INT");
     addMapping(POSTGRESQL, MSSQL, "INT2", "INT");
   }
 
   private void createMssqlToMysql() {
-    addMapping(MSSQL, MYSQL, "NVARCHAR", "VARCHAR(255)");
+    addMapping(MSSQL, MYSQL, "NVARCHAR", "VARCHAR", "(255)");
     addMapping(MSSQL, MYSQL, "VARBINARY", "BLOB");
   }
 
   private void createMssqlToPostgres() {
     addMapping(MSSQL, POSTGRESQL, "NVARCHAR", "TEXT");
     addMapping(MSSQL, POSTGRESQL, "VARBINARY", "BYTEA");
+  }
+
+  private static class ColumnDefinition {
+    private final String type;
+    private final String precision;
+
+    public ColumnDefinition(final String type, final String precision) {
+      this.type = type;
+      this.precision = precision;
+    }
+
+    public String getType() {
+      return type;
+    }
+
+    public String getPrecision() {
+      return precision;
+    }
   }
 }
